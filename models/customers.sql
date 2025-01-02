29c59adaa32d765453fc72a37028f242c8f44b6e
Lannah Saldana <lhbsaldana@gmail.com>
2025-01-02 05:23:09 +0000

    added documentation

--- models/customers.sql
+++ models/customers.sql
@@ -1,48 +1,19 @@
 
-{{
-  config(
-    materialized='view'
-  )
-}}
-
-
 with customers as (
+    select * from {{ref('stg_customers')}}), 
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-),
-
-customer_orders as (
-
+    orders as (
+        select * from {{ref('stg_orders')}}), 
+    
+    customer_orders as (
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
     from orders
-
-    group by 1
-
-),
+    group by 1),
 
 final as (
 
